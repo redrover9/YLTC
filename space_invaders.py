5ae999b282fff6c5acb51a104e8ffa7af9a71bc2
Grace Thompson <grace.thompson97@gmail.com>
2021-08-21 19:15:06 -0700

    add music for real this time

--- space_invaders.py
+++ space_invaders.py
@@ -22,6 +22,10 @@ class SpaceInvaders:
 
     def __init__(self):
         pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load("/Users/grace/Downloads/space_invaders/YLTC/music/song.ogg")
+        pygame.mixer.music.play(-1)
+
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
