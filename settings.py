8bcc5e385f677274ee91f29998982a262894fd6d
Grace Thompson <grace.thompson97@gmail.com>
2021-08-21 21:24:04 -0700

    speed demon

--- settings.py
+++ settings.py
@@ -4,14 +4,14 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (0, 0, 0)
-        self.ship_speed = 2.0
+        self.ship_speed = 5.0
         self.ship_limit = 3
-        self.bullet_speed = 2.0
+        self.bullet_speed = 7.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (195, 195, 195)
         self.bullets_allowed = 5
-        self.alien_speed = 1.5
+        self.alien_speed = 3.5
         self.fleet_drop_speed = 5
         self.fleet_direction = 1
         self.speedup_scale = 1.25
