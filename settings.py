30b348dfd97cf2eb3846118d8efe4e729576b3b3
Grace Thompson <grace.thompson97@gmail.com>
2021-08-21 19:41:50 -0700

    increase speed

--- settings.py
+++ settings.py
@@ -4,17 +4,17 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 700
         self.bg_color = (0, 0, 0)
-        self.ship_speed = 0.75
+        self.ship_speed = 2.0
         self.ship_limit = 3
-        self.bullet_speed = 1.0
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (195, 195, 195)
         self.bullets_allowed = 5
-        self.alien_speed = 0.5
-        self.fleet_drop_speed = 3
+        self.alien_speed = 1.5
+        self.fleet_drop_speed = 5
         self.fleet_direction = 1
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.25
         self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
