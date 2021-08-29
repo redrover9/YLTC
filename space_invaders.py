711e50b6943045f745e210b7fedfca5a8c0ebb92
Grace Thompson <grace.thompson97@gmail.com>
2021-08-29 13:31:16 -0700

    mute

--- space_invaders.py
+++ space_invaders.py
@@ -22,10 +22,10 @@ class SpaceInvaders:
 
     def __init__(self):
         pygame.init()
-        pygame.mixer.init()
-        pygame.mixer.music.set_volume(0.03)
-        pygame.mixer.music.load("music/song.ogg")
-        pygame.mixer.music.play(-1)
+        #pygame.mixer.init()
+        #pygame.mixer.music.set_volume(0.03)
+        #pygame.mixer.music.load("music/song.ogg")
+        #pygame.mixer.music.play(-1)
 
         self.settings = Settings()
 
