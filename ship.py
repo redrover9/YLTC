90f807900f9262a4477e163750a2c89d62d9d14f
Grace Thompson <grace.thompson97@gmail.com>
2021-08-29 13:15:20 -0700

    vertical

--- ship.py
+++ ship.py
@@ -14,18 +14,28 @@ class Ship(Sprite):
         self.rect.midbottom = self.screen_rect.midbottom
 
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y -= self.settings.ship_speed
         self.rect.x = self.x
+        self.rect.y = self.y
     def blitme(self):
         self.screen.blit(self.image, self.rect)
     def center_ship(self):
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
